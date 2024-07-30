e73ca1097743c1000c8dd42bc1a15c94cda2d9fe
MuhammedIkbalAKGUNDOGDU <muhammedikbalcmp@gmail.com>
2024-07-30 11:19:54 +0300

    banner finished

--- Flo_Copy/src/Components/banner.jsx
+++ Flo_Copy/src/Components/banner.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Banner = () => {
   const [banner, setBanner] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const intervalId = useRef(null);
 
   useEffect(() => {
     setBanner([
@@ -15,16 +16,33 @@ const Banner = () => {
   }, []);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
+    startInterval();
+
+    return () => clearInterval(intervalId.current);
+  }, [banner.length]);
+
+  const startInterval = () => {
+    clearInterval(intervalId.current);
+    intervalId.current = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % banner.length);
     }, 3000);
+  };
 
-    return () => clearInterval(intervalId);
-  }, [banner.length]);
+  const changeIndex = (indexNumber) => {
+    setCurrentIndex(indexNumber);
+    startInterval(); // Restart the interval when changing the index manually
+  };
 
   return (
     <div className="banner-container">
       {banner.length > 0 && <img src={banner[currentIndex]} alt="Banner" />}
+      <div className="banner-buttons">
+        <div onClick={() => changeIndex(0)} className={`buttons ${currentIndex === 0 ? 'active' : ''}`}></div>
+        <div onClick={() => changeIndex(1)} className={`buttons ${currentIndex === 1 ? 'active' : ''}`}></div>
+        <div onClick={() => changeIndex(2)} className={`buttons ${currentIndex === 2 ? 'active' : ''}`}></div>
+        <div onClick={() => changeIndex(3)} className={`buttons ${currentIndex === 3 ? 'active' : ''}`}></div>
+        <div onClick={() => changeIndex(4)} className={`buttons ${currentIndex === 4 ? 'active' : ''}`}></div>
+      </div>
     </div>
   );
 };
