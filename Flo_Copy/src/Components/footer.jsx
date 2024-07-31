5dc9c6ce0ff61d4e52000b2f7ddecffdcea89486
MuhammedIkbalAKGUNDOGDU <muhammedikbalcmp@gmail.com>
2024-07-31 11:41:14 +0300

    some footer elements added

--- Flo_Copy/src/Components/footer.jsx
+++ Flo_Copy/src/Components/footer.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import googleplay from "../assets/google_play.svg";
+import appstore from "../assets/app_store.svg";
+import appgallery from "../assets/app_gallery.svg";
+
+import { FaFacebook } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaPinterest } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
 
 const footer = () => {
   return (
     <div className="footer_top">
       <div className="musteri_hizmetleri">
-        <h4 style={{color:"gray"}}>Müşteri Hizmetleri</h4>
+        <h4 style={{ color: "gray" }}>Müşteri Hizmetleri</h4>
         <div className="underline underline2"></div>
 
         <div className="musteri_hizmetleri-2">
@@ -64,8 +73,43 @@ const footer = () => {
             <div className="underline"></div>
           </div>
         </div>
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: "30px",
+          }}
+        >
+          <img style={{ width: "140px" }} src={googleplay} alt="" />
+          <img style={{ width: "140px" }} src={appstore} alt="" />
+          <img style={{ width: "140px" }} src={appgallery} alt="" />
+        </div>
       </div>
-      <div className="follow_us"></div>
+      <div className="follow_us">
+        <h4 style={{ color: "gray" }}>Takip Edin</h4>
+        <div className="underline "></div>
+        <div style={{ marginTop: "1rem", fontSize: "19px" }}>
+          {" "}
+          <FaFacebook></FaFacebook> Flo Facebook
+        </div>
+        <div style={{ marginTop: "1rem", fontSize: "19px" }}>
+          {" "}
+          <FaXTwitter></FaXTwitter> Flo X
+        </div>
+        <div style={{ marginTop: "1rem", fontSize: "19px" }}>
+          {" "}
+          <FaPinterest></FaPinterest> Flo Pinterest
+        </div>
+        <div style={{ marginTop: "1rem", fontSize: "19px" }}>
+          {" "}
+          <FaSquareInstagram></FaSquareInstagram> Flo Instagram
+        </div>
+        <div style={{ marginTop: "1rem", fontSize: "19px" }}>
+          {" "}
+          <FaYoutube></FaYoutube> Flo Youtube
+        </div>
+      </div>
       <div className="third"></div>
     </div>
   );
